1a85ef716669a5ec95fe6d5d48e4b72288ab3657
Andreas Orthey <andreas.orthey@gmx.de>
2013-02-28 18:15:19 +0100

    removed burn in samples

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-f=open("../build/mcmc.tmp", "r");
+f=open("../mcmc.tmp", "r");
 lines=f.readlines();
 
 x=[]
@@ -18,9 +18,14 @@ xx=np.array(x)
 yy=np.array(y)
 dd=np.array(d, dtype=np.float64)
 
+burnin=50
+xx=xx[burnin:-1]
+yy=yy[burnin:-1]
+dd=dd[burnin:-1]
 
-p = plt.plot(xx[dd>0], yy[dd>0], 'og')
-plt.plot(xx[dd==0], yy[dd==0], 'ro')
+lim = 1
+p = plt.plot(xx[dd>lim], yy[dd>lim], 'og')
+plt.plot(xx[dd<lim], yy[dd<lim], 'ro')
 #plt.setp(p, 'markersize', 2)
 plt.show()
 
