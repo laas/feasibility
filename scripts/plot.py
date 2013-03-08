ebaa8745d17de5d4ddd313c05918b0c62cd984d0
Andreas Orthey <andreas.orthey@gmx.de>
2013-03-08 16:44:28 +0100

    multi plot of sampling process

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,33 +1,55 @@
 import numpy as np
-import matplotlib.pylab as plt
+from os import walk
+#import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
 
-f=open("../mcmc.tmp", "r");
-lines=f.readlines();
+files = []
+counter=0
+for (dirpath, dirname, filenames) in walk("../mcmc/"):
+	files.extend(filenames)
+	counter=counter+1
 
-x=[]
-y=[]
-d=[]
-for l in lines:
-	p=l.split()
-	x.append(p[0])
-	y.append(p[1])
-	d.append(p[2])
+for i in range(1,4):
+	files.append(files[0])
+	counter=counter+1
+
+print files
+
+pN=2
+for i in range(0,counter):
+	print "go"
+	sfile ="../mcmc/"+files[i]
+	f=open(sfile, "r")
+	lines=f.readlines()
+	plt.subplot(pN,pN,i+1)
+	x=[]
+	y=[]
+	z=[]
+	d=[]
+	for l in lines:
+		p=l.split()
+		x.append(p[0])
+		y.append(p[1])
+		z.append(p[2])
+		d.append(p[3])
 
 
-xx=np.array(x)
-yy=np.array(y)
-dd=np.array(d, dtype=np.float64)
+	xx=np.array(x)
+	yy=np.array(y)
+	zz=np.array(z)
+	dd=np.array(d, dtype=np.float64)
 
-burnin=50
-xx=xx[burnin:-1]
-yy=yy[burnin:-1]
-dd=dd[burnin:-1]
+	burnin=20
+	xx=xx[burnin:-1]
+	yy=yy[burnin:-1]
+	dd=dd[burnin:-1]
 
-lim = 1
-p = plt.plot(xx[dd>lim], yy[dd>lim], 'og')
-plt.plot(xx[dd<lim], yy[dd<lim], 'ro')
-#plt.setp(p, 'markersize', 2)
+	lim = 1
+	p = plt.plot(xx[dd>lim], yy[dd>lim], 'og')
+	plt.plot(xx[dd<lim], yy[dd<lim], 'ro')
+
 plt.show()
-
 
 
