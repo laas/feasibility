897a866a8c89ce595690747c5ecbaadce9b73fb5
Andreas Orthey <andreas.orthey@gmx.de>
2013-03-18 14:58:44 +0100

    cleaned code for footstep planner node

--- src/main.cpp
+++ src/main.cpp
@@ -24,20 +24,11 @@ int main( int argc, char** argv )
 	std::string chair_file = get_chair_str();
 	std::string robot_file = get_robot_str();
 
-	//######################################################
-	// fastPlanner
-	//######################################################
-	fastreplanning::FastReplanningInterface *planner 
-		= fastreplanning::fastReplanningInterfaceFactory(prefix, argc, argv);
-	planner->setVerboseLevel(15); //0 5 15
 
 	//######################################################
 	// set robot start
 	//######################################################
 	std::vector<double> start;
-	start.push_back(1.0);
-	start.push_back(-1.0);
-	start.push_back(0.0);
 	start.push_back(0.0);
 	start.push_back(0.0);
 	start.push_back(0.0);
@@ -49,7 +40,15 @@ int main( int argc, char** argv )
 	TriangleObject chair(chair_file, cx, cy, cz);
 	TriangleObject robot(robot_file, rx, ry, rz);
 
-	planner->mainLoop();
+	//######################################################
+	// fastPlanner
+	//######################################################
+	fastreplanning::FastReplanningInterface *planner 
+		= fastreplanning::fastReplanningInterfaceFactory(prefix, argc, argv);
+
+	planner->setVerboseLevel(15); //0 5 15
+	planner->mainLoop(); //init
+
 	planner->addObstacleFromDatabase(CHAIR, cx, cy, cz, 1,1,1); 
 	//planner->addObstacleFromDatabase(BOX, 0.49, 0.21, 0, 1,1,1); 
 	planner->updateLocalizationProtected(start);
@@ -59,10 +58,11 @@ int main( int argc, char** argv )
 	while (ros::ok())
 	{
 
-
 		std::vector<double> goal;
-		goal.push_back(1.6); goal.push_back(0.1); goal.push_back(0.0);
+		goal.push_back(2.6); goal.push_back(0.5); goal.push_back(0.0);
 		planner->update3DGoalPositionProtected(goal);
+
+		//Logger log("steps2.dat");
 
 		ROS_INFO("foot step planner START->");
 		planner->mainLoop();
@@ -75,62 +75,48 @@ int main( int argc, char** argv )
 		planner->getInterfaceSteps(fsi);
 		ROS_INFO("NUMBER OF FOOTSTEPS: %d", fsi.size());
 
-		double last_xL = 0;
-		double last_yL = 0;
-		double last_tL = 0;
-		double last_xR = 0;
-		double last_yR = 0;
-		double last_tR = 0;
+		double abs_x = 0.0;
+		double abs_y = 0.0;
+		double abs_t = 0.0;
 		for(uint i=0;i<fsi.size();i++){
-			if(i==0){
-				for(uint j=0;j<fso.size();j++){
-					//ROS_INFO("erasing step %d",j);
-					//ROS_INFO("id= %d", fso.at(j).id);
-					//%fso.at(j).remove();
-				}
-				//fso.clear();
-			}
 			//half-foot-step-format v.3.0:
 			// 1: x
 			// 2: y
 			// 3: theta
 			// 4: ascii code for L or R
 			// 5-11: do not know
+			// 
+			/*
 			for(uint j=0;j<fsi.at(i).data.size();j++){
 				printf("%f - ",fsi.at(i).data.at(j));
 			}
 			printf("\n");
-			double scale=1;
-			double x,y,t;
+			log("%f %f %f %f %f %f %f %f %f %f %f", fsi.at(i).data.at(0), fsi.at(i).data.at(1), 
+					fsi.at(i).data.at(2), fsi.at(i).data.at(3),
+					fsi.at(i).data.at(4), fsi.at(i).data.at(5),
+					fsi.at(i).data.at(6), fsi.at(i).data.at(7),
+					fsi.at(i).data.at(8), fsi.at(i).data.at(9),
+					fsi.at(i).data.at(10)
+					);
+			*/
+			///////////
+			double x = fsi.at(i).data.at(0);
+			double y = fsi.at(i).data.at(1);
+			double t = fsi.at(i).data.at(2);
+			char foot = fsi.at(i).data.at(3);
 
-//double radV = -state_prev->x[2]*PI/180.0;
-//s.x = cos(radV)*(state_curr->x[0] - state_prev->x[0]) - 
-//  sin(radV)*(state_curr->x[1] - state_prev->x[1]);
-//s.y = sin(radV)*(state_curr->x[0] - state_prev->x[0]) + 
-//  cos(radV)*(state_curr->x[1] - state_prev->x[1]);
-//s.theta = (state_curr->x[2] - state_prev->x[2])*PI/180.0;
-//s.f = state_prev->x[3];
+			double newX = abs_x + cos(abs_t)*x-sin(abs_t)*y;
+			double newY = abs_y + sin(abs_t)*x+cos(abs_t)*y;
+			double newT = abs_t + t;
+			FootStepObject f(i, newX,newY,newT);
+			abs_x=newX;
+			abs_y=newY;
+			abs_t=newT;
 
-			double 
-			if(fsi.at(i).data.at(3) == 'R'){
-				x = fsi.at(i).data.at(0)+last_xL;
-				y = fsi.at(i).data.at(1)+last_yL;
-			}else{
-				x = fsi.at(i).data.at(0)+last_xR;
-				y = fsi.at(i).data.at(1)+last_yR;
-			}
-			FootStepObject f(i,scale*x,scale*y,scale*t);
-
-			if(fsi.at(i).data.at(3) == 'R'){
+			if(foot == 'R'){
 				f.changeColor(1,0,0);
-				f.drawLine(scale*last_xR, scale*last_yR);
-				last_xR = x;
-				last_yR = y;
 			}else{
 				f.changeColor(0,1,0);
-				f.drawLine(scale*last_xL, scale*last_yL);
-				last_xL = x;
-				last_yL = y;
 			}
 
 			if(i==0 || i==1){
@@ -141,8 +127,7 @@ int main( int argc, char** argv )
 			}
 
 			f.rviz_publish();
-			fso.push_back(f);
-			ROS_INFO("new footstep [%d] at x=%f, y=%f, theta=%f", i,x,y,theta);
+			ROS_INFO("new footstep [%d] at x=%f, y=%f, theta=%f", i,newX,newY,newT);
 		}
 
 		//######################################################
