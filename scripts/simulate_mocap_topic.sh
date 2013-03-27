05b7540e136ed8b8eac09b90b9614e97fb122893
Andreas Orthey <andreas.orthey@gmx.de>
2013-03-27 14:50:16 +0100

    minor revision

--- scripts/simulate_mocap_topic.sh
+++ scripts/simulate_mocap_topic.sh
@@ -1,2 +1,2 @@
-rostopic pub /evart/helmet/origin geomry_msgs/TransformStamped '{header: {frame_id: 'mocap_world'}, child_frame_id: 'id', transform: {translation: {x: 1., y: 0., z: 0.}, rotation: {x: 0., y: 0., z: 0., w: 1.}}}'
-rostopic pub /evart/chair/origin geomry_msgs/TransformStamped '{header: {frame_id: 'mocap_world'}, child_frame_id: 'id', transform: {translation: {x: -1., y: 0., z: 0.}, rotation: {x: 0., y: 0., z: 0., w: 1.}}}'
+rostopic pub -1 /evart/helmet/origin geometry_msgs/TransformStamped '{header: {frame_id: 'mocap_world'}, child_frame_id: 'id', transform: {translation: {x: 1., y: 0., z: 0.}, rotation: {x: 0., y: 0., z: 0., w: 1.}}}'
+rostopic pub -1 /evart/chair/origin geometry_msgs/TransformStamped '{header: {frame_id: 'mocap_world'}, child_frame_id: 'id', transform: {translation: {x: -1., y: 0., z: 0.}, rotation: {x: 0., y: 0., z: 0., w: 1.}}}'
