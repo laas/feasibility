46907382ae828e1d99665c20d690063cfe2b33ac
Andreas Orthey <andreas.orthey@gmx.de>
2013-04-01 18:19:33 +0200

    revised install script

--- install.sh
+++ install.sh
@@ -3,38 +3,50 @@
 ######## Change lines before executing script:
 
 ### trac.laas.fr username
-USERNAME=aorthey
+USERNAME_SYS=orthez
+USERNAME_LAAS=aorthey
 
 ### install dir for openrobots/robotpkg etc
-INSTALL_DIR=/home/aorthey
+INSTALL_DIR=/opt
 
 ### installing the git libraries
-LIB_DIR=/home/aorthey/lib
+LIB_DIR=/home/${USERNAME_SYS}/lib
 
 ### where to download git archives
-GIT_DIR=/home/aorthey/git
+GIT_DIR=/home/${USERNAME_SYS}/git
 #########################################################################
 #########################################################################
-
-
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:${LIB_DIR}/lib/pkgconfig:${INSTALL_DIR}/openrobots/lib/pkgconfig
 
+##### reload robotpkg.conf
+rm -rf ${INSTALL_DIR}/openrobots/etc/robotpkg.conf
+echo "ACCEPTABLE_LICENSES+=pqp-license" >> ${INSTALL_DIR}/openrobots/etc/robotpkg.conf
+echo "ACCEPTABLE_LICENSES+=cnrs-hpp-closed-source" >> ${INSTALL_DIR}/openrobots/etc/robotpkg.conf
+
+###### Load all packages into GIT folder
+cd ${GIT_DIR}
+git clone ssh://${USERNAME_LAAS}@trac.laas.fr/git/jrl/rblink/fastReplanningData.git
+git clone ssh://${USERNAME_LAAS}@trac.laas.fr/git/jrl/rblink/analyticalPG
+git clone ssh://${USERNAME_LAAS}@trac.laas.fr/git/jrl/algo/multiRRT.git
+git clone git://github.com/flexible-collision-library/fcl.git
+git clone git://git.openrobots.org/robots/robotpkg
+git clone git@github.com:danfis/libccd.git
+
+echo "Successfully downloaded all packages"
 #########################################################################
 ## robot-pkg
 #########################################################################
 cd ${GIT_DIR}
-git clone git://git.openrobots.org/robots/robotpkg
 cd robotpkg/bootstrap
-./bootstrap --prefix=${HOME}/openrobots
+./bootstrap --prefix=${GIT_DIR}/openrobots
 
 #########################################################################
 ## analyticalPG
 #########################################################################
 cd ${GIT_DIR}
-git clone ssh://${USERNAME}@trac.laas.fr/git/jrl/rblink/analyticalPG
 cd analyticalPG
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX:PATH=~/lib
+cmake .. -DCMAKE_INSTALL_PREFIX:PATH=${LIB_DIR}
 make -j5
 make all install
 
@@ -42,31 +54,28 @@ make all install
 ## multiRRT
 #########################################################################
 cd ${GIT_DIR}
-git clone ssh://${USERNAME}@trac.laas.fr/git/jrl/algo/multiRRT.git
 cd multiRRT
-cmake .. -DCMAKE_INSTALL_PREFIX:PATH=~/lib
+cmake .. -DCMAKE_INSTALL_PREFIX:PATH=${LIB_DIR}
 make all install
 
 #########################################################################
 ##LIBCCD (required for FCL)
 #########################################################################
 cd ${GIT_DIR}
-git clone git@github.com:danfis/libccd.git
 cd libccd
 ./bootstrap
-./configure --prefix=/home/aorthey/lib
+./configure --prefix=${LIB_DIR}
 mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX:PATH=~/lib
+cmake .. -DCMAKE_INSTALL_PREFIX:PATH=${LIB_DIR}
 make all install
 
 #########################################################################
 ## FCL
 #########################################################################
 cd ${GIT_DIR}
-git clone git://github.com/flexible-collision-library/fcl.git
 cd fcl
 mkdir build
-cmake .. -DCMAKE_INSTALL_PREFIX:PATH=~/lib
+cmake .. -DCMAKE_INSTALL_PREFIX:PATH=${LIB_DIR}
 make -j5 all install
 
 #########################################################################
@@ -80,13 +89,9 @@ cd ${GIT_DIR}/robotpkg/motion/dynamic-graph-corba
 make update
 cd ${GIT_DIR}/robotpkg/net/evart-client
 make update
-echo "ACCEPTABLE_LICENSES+=cnrs-hpp-closed-source" >> ${INSTALL_DIR}/openrobots/etc/robotpkg.conf
 cd ${GIT_DIR}/robotpkg/math/hrp2-dynamics
 make update
-echo "ACCEPTABLE_LICENSES+=pqp-license" >> ${INSTALL_DIR}/openrobots/etc/robotpkg.conf
 cd ${GIT_DIR}/robotpkg/graphics/pqp
 make update
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${INSTALL_DIR}/openrobots:${LIB_DIR}/lib
 
-cd ~/git
-git clone ssh://aorthey@trac.laas.fr/git/jrl/rblink/fastReplanningData.git
