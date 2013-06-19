eadacf2b9586fe3153a94d2dc386b6170796e393
Andreas Orthey <andreas.orthey@gmx.de>
2013-06-19 18:41:16 +0200

    minor

--- src/environment_13humanoids_real.cpp
+++ src/environment_13humanoids_real.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>    // std::min
 #include "environment.h"
 
 void Environment13HumanoidsReal::setDecorations(){
@@ -14,30 +15,34 @@ void Environment13HumanoidsReal::setObjects(){
 
 	//ros::TriangleObjectFloor *chair = new ros::TriangleObjectFloor(0.8, 0.5, "chairLabo.tris", "/evart/chair2/PO");
 	//c=new ros::CylinderMarkerTriangles(1,0, rand(0.1,0.1), rand(0.5,0.5));
-	c=new ros::CylinderMarkerTriangles(1,0, rand(0.3,0.3), rand(0.5,0.5));
+	/*
+	c=new ros::CylinderMarkerTriangles(1,-0.9, rand(0.1,0.1), rand(0.5,0.5));
 	c->set_color(0,0.8,0,0.2);
 	c->make_interactive();
 	objects.push_back(c);
 
-	c=new ros::CylinderMarkerTriangles(2,0, rand(0.3,0.3), rand(0.5,0.5));
+	c=new ros::CylinderMarkerTriangles(1.5,0.7, rand(0.1,0.1), rand(0.5,0.5));
 	c->set_color(0,0.8,0,0.2);
 	c->make_interactive();
 	objects.push_back(c);
 
-	c=new ros::CylinderMarkerTriangles(3,0, rand(0.3,0.3), rand(0.5,0.5));
+	c=new ros::CylinderMarkerTriangles(2.5,-0.5, rand(0.1,0.1), rand(0.5,0.5));
 	c->set_color(0,0.8,0,0.2);
 	c->make_interactive();
 	objects.push_back(c);
+	*/
 	//RADIUS \elem [0.01, 0.1]
 	//HEIGHT \elem [0.01, 0.5]
 	// otherwise the MLP function is undefined
-	/*
 	for(uint i=0;i<Nobjects;i++){
 		c=new ros::CylinderMarkerTriangles(rand(0.2,2.8),rand(-1.5,1.5), rand(0.01,0.04), rand(0.01,0.06));
 		c->set_color(1.0,0.3,0.3,0.3);
-		c->make_interactive();
 		objects.push_back(c);
 	}
+	for(uint i=0;i< std::min((int)objects.size(),5);i++){
+		objects.at(i)->make_interactive();
+	}
+	/*
 	for(uint i=0;i<10;i++){
 		c=new ros::CylinderMarkerTriangles(rand(0.2,2.8),rand(-1.5,1.5), rand(0.01,0.08), rand(0.1,0.5));
 		c->set_color(0,0.8,0,0.3);
