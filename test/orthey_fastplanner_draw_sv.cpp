e7fb751a4d22f18db5006dace7bdde2e4115a446
Andreas Orthey <andreas.orthey@gmx.de>
2013-10-10 14:49:12 +0200

    fastplanner exe which draws swept volume to rviz

---
+++ test/orthey_fastplanner_draw_sv.cpp
@@ -0,0 +1,45 @@
+#include <ros/ros.h>
+#include <ros/time.h>
+#include <std_msgs/String.h>
+#include <visualization_msgs/Marker.h>
+#include <vector> //std::vector
+#include <iostream> //cout
+
+#include "rviz/visualmarker.h"
+#include "util/util.h"
+#include "environment/environment.h"
+#include "planner/motionplanner_astar.h"
+#include "planner/constraints_checker_ann.h"
+
+using namespace ros;
+int main( int argc, char** argv )
+{
+	ros::init(argc, argv, "orthey_sv_drawer");
+	ros::NodeHandle n;
+	ros::Rate r(1);
+
+	Environment* environment;
+	MotionPlannerAStar *astar;
+
+	environment->Nobjects = 60;
+	environment = Environment::get13HumanoidsReal();
+	astar = new MotionPlannerAStar(environment, argc, argv);
+
+	ConstraintsChecker *cc = new ConstraintsCheckerANN(16);
+	astar->setConstraintsChecker(cc);
+
+	while (ros::ok())
+	{
+		astar->plan();
+		if(astar->success()){
+			astar->clean_publish();
+			astar->update_planner();
+			astar->publish_onestep_next();
+			astar->publish_onestep_next();
+			astar->draw_swept_volume();
+			return 0;
+		}
+	}
+}
+
+
