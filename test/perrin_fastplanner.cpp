338e194893d66e28746a1d9707433937e459309d
Andreas Orthey <andreas.orthey@gmx.de>
2013-11-08 14:52:48 +0100

    rostopic pub -1 /planner/stop std_msgs/Bool '{data: false}'

--- test/perrin_fastplanner.cpp
+++ test/perrin_fastplanner.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <ros/time.h>
 #include <std_msgs/String.h>
+#include <std_msgs/Bool.h>
 #include <visualization_msgs/Marker.h>
 #include <vector> //std::vector
 #include <iostream> //cout
@@ -13,11 +14,20 @@
 #include "planner/constraints_checker_ann.h"
 
 using namespace ros;
+bool plan;
+
+void update(const std_msgs::Bool& stopPlanner){
+	plan = !stopPlanner.data;
+}
+
 int main( int argc, char** argv )
 {
 	ros::init(argc, argv, "perrin_fast_replanner");
 	ros::NodeHandle n;
 	ros::Rate r(1);
+	ros::Subscriber subscriber;
+
+	subscriber = n.subscribe("/planner/stop", 100, &update);
 
 	Environment* environment;
 	MotionPlannerAStar *astar;
@@ -30,11 +40,16 @@ int main( int argc, char** argv )
 
 	while (ros::ok())
 	{
-		astar->plan();
-		if(astar->success()){
-			astar->clean_publish();
-			astar->update_planner();
-			astar->publish_onestep_next();
+		if(plan){
+			astar->plan();
+			if(astar->success()){
+				astar->clean_publish();
+				astar->update_planner();
+				astar->publish_onestep_next();
+			}
+		}else{
+			r.sleep();
+			ROS_INFO("Planner in STOPPING mode (change it on /planner/stop)");
 		}
 	}
 }
