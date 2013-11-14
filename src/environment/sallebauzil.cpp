3eab552a8831345f77c492e0cd316d792f0441d0
Olivier Stasse <olivier.stasse@gmail.com>
2013-11-14 15:42:48 +0100

    Update blue_column + setup for Airbus demonstration.

--- src/environment/sallebauzil.cpp
+++ src/environment/sallebauzil.cpp
@@ -1,21 +1,27 @@
-#include "environment/environment.h"
+ #include "environment/environment.h"
 
-void EnvironmentSalleBauzil::setDecorations(){
-  ros::RVIZVisualMarker *c;
-  c = new ros::ColladaObject("package://feasibility/data/wall_laas9.obj");
+ void EnvironmentSalleBauzil::setDecorations(){
+   ros::RVIZVisualMarker *c;
+   c = new ros::ColladaObject("package://feasibility/data/wall_laas9.obj");
 
-  //c->setXYZ(1.5,-2.5,-0.01);
-  c->setXYZ(0.0,0,-0.01);
-  //c->setRPYRadian(0,0,M_PI);
+   c->setXYZ(0.0,0,-0.01);
 
-  decorations.push_back(c);
+   decorations.push_back(c);
 
-  c = new ros::ColladaObject("package://feasibility/data/blue_column.obj");
-  c->setXYZ(3.3,0.3,-0.01);
-  decorations.push_back(c);
+   c = new ros::ColladaObject("package://feasibility/data/blue_column.obj");
+   c->setXYZ(0.3,3.3,-0.01);
+   decorations.push_back(c);
 
-  c = new ros::TriangleObjectFloor(0.0, 0.0, "data/wall.tris", "/evart/chair2/PO", std::string("fastReplanningData"));
-  decorations.push_back(c);
+   c = new ros::ColladaObject("package://feasibility/data/blue_column.obj");
+   c->setXYZ(-3.795,3.3,-0.01);
+   decorations.push_back(c);
+
+   c = new ros::ColladaObject("package://feasibility/data/blue_column.obj");
+   c->setXYZ(4.96,3.3,-0.01);
+   decorations.push_back(c);
+
+  //c = new ros::TriangleObjectFloor(0.0, 0.0, "data/wall.tris", std::string("fastReplanningData"));
+  //  decorations.push_back(c);
 
 }
 
@@ -44,14 +50,14 @@ void EnvironmentSalleBauzil::setObjects(){
   //ros::BlenderMeshTriangleObject *chair = new ros::BlenderMeshTriangleObject("package://feasibility/data/AluminumChair.dae","chairLabo.tris",1.2,0.5,M_PI);
 }
 void EnvironmentSalleBauzil::setGoalObject(){
-  goal = new ros::SphereMarker(2.5, 0.0, 0.2, 0.0);
+  goal = new ros::SphereMarker(-2.75, 1.75, 0.2, 0.0);
   //goal->setRPYRadian(0,0,M_PI);
   goal->make_interactive(0.3);
   goal->addText("goal");
   //goal->subscribeToEvart("/evart/red_airbus_screw/PO");
 }
 void EnvironmentSalleBauzil::setStartObject(){
-  start = new ros::SphereMarker(-2.0, -1.0, 0.2, 0.0);
+  start = new ros::SphereMarker(2.0, -0.75, 0.2, 0.0);
   start->addText("start");
   //start->subscribeToEvart("/evart/robot/PO");
 }
